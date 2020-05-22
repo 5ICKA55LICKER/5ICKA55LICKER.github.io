56cf5bf48a80f272b4afe20fee3726fb5a6fa78f
blixt <teodor.blixt@gmail.com>
2020-05-22 13:26:11 +0200

    fixed navigation

---
+++ precache-manifest.f98db829daacb2c23359de9b5d7ecb8b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7042594c049d25948832378c74908a33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e50edc54834ab7115499",
+    "url": "/static/css/main.dc88ecce.chunk.css"
+  },
+  {
+    "revision": "6ded0f01d69fd0c8eae3",
+    "url": "/static/js/2.66c609c7.chunk.js"
+  },
+  {
+    "revision": "296362f3435e3c4e1832bddadf6a9199",
+    "url": "/static/js/2.66c609c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48774af18fcb8edbe52d",
+    "url": "/static/js/3.37ee5380.chunk.js"
+  },
+  {
+    "revision": "afb4576f927ccaaa7eef",
+    "url": "/static/js/4.3fc4a457.chunk.js"
+  },
+  {
+    "revision": "310e0d5ea470723c23ff",
+    "url": "/static/js/5.fa5438c7.chunk.js"
+  },
+  {
+    "revision": "f1dffb2137cb20ecfdb6",
+    "url": "/static/js/6.054e8925.chunk.js"
+  },
+  {
+    "revision": "e50edc54834ab7115499",
+    "url": "/static/js/main.944e58bd.chunk.js"
+  },
+  {
+    "revision": "35050f9e8a94734a04b0",
+    "url": "/static/js/runtime-main.a57308a9.js"
+  }
+]);
